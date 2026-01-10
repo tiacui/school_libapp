3f633c474d30d9c1a3ebd331ec51be6272ea4801
Your Name <you@example.com>
2026-01-10 16:49:45 +1100

    xxx

--- school_libapp.py
+++ school_libapp.py
@@ -1,4 +1,7 @@
+import sys
 import datetime
+import json
+
 
 
 
@@ -18,8 +21,8 @@ def userlogin():
     file = "config/credentials.txt"
     
     while True:
-        username = input("enter username: ")
-        password = input("enter your password: ")
+        username = input("username: ")
+        password = input("password: ")
 
         with open(file, "r") as filedata:
             for line in filedata.readlines():
@@ -35,9 +38,48 @@ def userlogin():
                     log(username, "success")
                     return username
         log(username, "fail1")
+
+    
     
               
 
 
 user = userlogin()
 print("you are: ", user)
+
+if user == "admin":
+    print("\n\n***********************************")
+    print("* 1) list all books")
+    print("* 2) add books")
+    print("* 3) remove books")
+    print("* q) quit")
+    print("***********************************")
+
+    select = input("please select a number 1-3: ")
+    if select == "1":
+        print("listing all books here")
+    elif select == "2":
+        print("add books here")
+    elif select == "3":
+        print("remove books here")
+    elif select == "q":
+        print("quit...")
+        sys.exit()
+    else:
+        print("invalid number")
+elif user == "student1" or "student2":
+    print("***********************************")
+    print("* 1) list all books")
+    print("* 2) borrow a book")
+    print("* 3) return a book")
+    print("***********************************")
+
+    select = input("select a number 1-3:")
+    if select == "1":
+        print("listing all books here")
+    elif select == "2":
+        print("borrowing books here")
+    elif select == "3":
+        print("returning books here")
+    else:
+        print("invalid number")
