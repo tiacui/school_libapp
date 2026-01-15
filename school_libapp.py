cc230c8ba303f3ecad0d8d3f09dfe47c06b0b5bc
Your Name <you@example.com>
2026-01-15 14:50:00 +1100

    xxx

--- school_libapp.py
+++ school_libapp.py
@@ -1,98 +1,64 @@
 import sys
-import datetime
-import json
-#from school_library import SchoolLibrary
 import school_library
 
 
+def main():
+
+    user = school_library.user_login()
+    print("you are: ", user)
+    car_library = school_library.SchoolLibrary("caringbah")
+    car_library.load_data()
 
 
-
-
-
-
-def log(name, result):
-    with open("log/log.txt", "a") as logfile:
-        cur_time = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%dT%H:%M:%SZ')
-        logfile.write(cur_time + " " + name + " " + result + "\n")
-
-
-
-
-def userlogin():
-    file = "config/credentials.txt"
-    
     while True:
-        username = input("username: ")
-        password = input("password: ")
+        if user == "admin":
+            print("\n\n***********************************")
+            print("* 1) list all books")
+            print("* 2) add books")
+            print("* 3) remove books")
+            print("* q) quit")
+            print("***********************************")
 
-        with open(file, "r") as filedata:
-            for line in filedata.readlines():
-                line = line.strip()
-
-                user_and_password = line.split(":")
-
-                username123 = user_and_password[0]
-                password123 = user_and_password[1]
-
-                if username == username123 and password == password123:
-                    print("login successful")
-                    log(username, "success")
-                    return username
-        log(username, "fail1")
-
-    
-    
+            select = input("please select a number 1-3: ")
+            if select == "1":
+                print("listing all books here")
+                car_library.list_books(user)
+                
+            elif select == "2":
+                print("add books here")
+                car_library.add_book(user)
               
+            elif select == "q":
+                print("quit...")
+              
+                sys.exit()
+            else:
+                print("invalid number")
+        elif user == "student1" or user == "student2":
+            print("***********************************")
+            print("* 1) list all books")
+            print("* 2) borrow a book")
+            print("* 3) return a book")
+            print("* q) quit")
+            print("***********************************")
+
+            select = input("select a number 1-3:")
+            if select == "1":
+                print("listing all books here")
+                car_library.list_books(user)
+            elif select == "2":
+                print("borrowing books here")
+                car_library.borrow_book(input('book_id: '))
+            elif select == "3":
+                print("returning books here")
+                car_library.return_book(input('book_id: '))
+            elif select == "q":
+                print("quit...")
+                sys.exit()
+            
+            else:
+                print("invalid number")
 
 
-user = userlogin()
-print("you are: ", user)
-car_library = school_library.SchoolLibrary("caringbah")
-car_library.load_data()
-
-while True:
-    if user == "admin":
-        print("\n\n***********************************")
-        print("* 1) list all books")
-        print("* 2) add books")
-        print("* 3) remove books")
-        print("* q) quit")
-        print("***********************************")
-
-        select = input("please select a number 1-3: ")
-        if select == "1":
-            print("listing all books here")
-            car_library.list_books()
-        elif select == "2":
-            print("add books here")
-            car_library.add_book()
-        elif select == "q":
-            print("quit...")
-            sys.exit()
-        else:
-            print("invalid number")
-    elif user == "student1" or "student2":
-        print("***********************************")
-        print("* 1) list all books")
-        print("* 2) borrow a book")
-        print("* 3) return a book")
-        print("* q) quit")
-        print("***********************************")
-
-        select = input("select a number 1-3:")
-        if select == "1":
-            print("listing all books here")
-            car_library.list_books()
-        elif select == "2":
-            print("borrowing books here")
-            car_library.borrow_book(input('book_id: '))
-        elif select == "3":
-            print("returning books here")
-            car_library.return_book(input('book_id: '))
-        elif select == "q":
-            print("quit...")
-            sys.exit()
-        
-        else:
-            print("invalid number")
+if __name__ == '__main__':
+    main()
--- school_library.py
+++ school_library.py
@@ -1,6 +1,37 @@
 import json
 import pprint
 import random
+import datetime
+
+
+def log(name, message):
+    with open("log/log.txt", "a") as logfile:
+        cur_time = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%dT%H:%M:%SZ')
+        logfile.write(cur_time + " " + name + " " + message + "\n")
+
+
+def user_login():
+    file = "config/credentials.txt"
+    
+    while True:
+        username = input("username: ")
+        password = input("password: ")
+
+        with open(file, "r") as filedata:
+            for line in filedata.readlines():
+                line = line.strip()
+
+                user_and_password = line.split(":")
+
+                username123 = user_and_password[0]
+                password123 = user_and_password[1]
+
+                if username == username123 and password == password123:
+                    print("login successful")
+                    log(username, "success")
+                    return username
+        log(username, "fail")
+
 
 class SchoolLibrary:
     def __init__(self, name,):
@@ -19,16 +50,17 @@ class SchoolLibrary:
             json.dump(self.books, loaddata)
             print("data saved")
 
-    def list_books(self):
+    def list_books(self, username):
         print("listing books")
-        print("book_id     author    name     stocks")
+        print("book_id     author     name      stocks")
+        log(username, "is listing books")
         for book in self.books:
 
             print(book['book_id'], "    ",book['author'], "    ", book['name'], "    ", book['stock'])
             print()
 
     
-    def add_book(self):
+    def add_book(self, username):
         print("adding books")
         book_id = random.randint(100000,999999)
         name = input("name: ")
@@ -36,43 +68,27 @@ class SchoolLibrary:
         stock = input("stock: ")
         
         new_book = {"book_id": book_id, "name": name, "author": author, "stock": int(stock)}
+        log(username, "book added")
 
         self.books.append(new_book)
         self.save_data()
 
 
     
-    def borrow_book(self, book_id):
+    def borrow_book(self, book_id, username):
         print("borrow " + book_id)
         for book in self.books:
             if book["book_id"] == int(book_id):
                 book["stock"] = book["stock"] - 1
                 print("book borrowed successfully")
+                log(username, "book borrowed")
                 self.save_data()
 
-    def return_book(self, book_id):
+    def return_book(self, book_id, username):
         print("returned " + book_id)
         for book in self.books:
             if book["book_id"] == int(book_id):
                 book["stock"] = book["stock"] + 1
                 print("book returned successfully")
+                log(username, "book returned")
                 self.save_data()
-
-
-    
-    
-
-def main():
-    car_library = SchoolLibrary("caringbah")
-    car_library.load_data()
-    print("my library is", car_library.name)
-    print(pprint.pprint(car_library.books))
-    car_library.borrow_book(input('book_id: '))
-
-    #library.books[0]['name'] = "hello"
-    #library.save_data()
-
-
-
-if __name__ == '__main__':
-    main()
